7977c17f3455cf8bc73c9400f4993553d7be8195
zunaisha011 <zunaishajavaid10@gmail.com>
2025-08-03 21:08:16 +0500

    Update open.py

--- ReadAndWrite/open.py
+++ ReadAndWrite/open.py
@@ -10,7 +10,12 @@
 # b.close()
 
 
-z=(r'C:\Users\Hp\Desktop\Python\file.txt','r')
-with open(r'C:\Users\Hp\Desktop\Python\file.txt','r') as z:
-    n=z.read()
+with open(r'C:\Users\Hp\Desktop\Python\file.txt', 'r') as z:
+    n = z.read()
     print(n)
+
+
+
+with open(r'C:\Users\Hp\Desktop\Python\file.txt', 'r') as k:
+    file=k.read()
+    print(file)
